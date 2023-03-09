cfa2e0b76cecd29a826bbf1ba6930b27b63b7118
ZYC0923 <a90092331@gmail.com>
2023-03-09 15:32:26 +0800

    測試

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'pj1bg23', 'tags': '', 'url': 'About.html'}]};
